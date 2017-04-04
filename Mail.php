520aa55a4e822da51e8f7ba0bf320b68c59601af
Nyk Secosky <nsecosky@gmail.com>
2017-04-04 08:25:31 -0600

    Add files via upload
    Stupid changes I forgot to do

--- Mail.php
+++ Mail.php
@@ -4,13 +4,11 @@
   //Email information
   $admin_email = "someone@example.com";
   $email = $_REQUEST['email'];
-  $subject = $_REQUEST['subject'];
-  $comment = $_REQUEST['comment'];
+  $subject = "You Have A Pckage";
+  $comment = "You have package(s) waiting for you. Packages are available for pick up during normal Desk Operational Hours. PLEASE NOTE! MavCard's are required to check out all packages!";
   
   //send email
   mail($admin_email, "$subject", $comment, "From:" . $email);
-  
-  //Email response
-  echo "You have a Package waiting at the front desk! You can come pick it up during normal Desk Hours. MavCards are Required for checkout.";
+ 
   }
 ?>
