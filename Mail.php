7930710b29655418cf26c0894a54ac57c0beb1c1
nsecosky <nsecosky@gmail.com>
2017-04-17 14:10:36 -0600

    Added some tv stuff

--- Mail.php
+++ Mail.php
@@ -1,14 +1,13 @@
 <?php
  (isset($_REQUEST['email']))  {
-  
+
   //Email information
-  $admin_email = "someone@example.com";
   $email = $_REQUEST['email'];
   $subject = "You Have A Pckage";
   $comment = "You have package(s) waiting for you. Packages are available for pick up during normal Desk Operational Hours. PLEASE NOTE! MavCard's are required to check out all packages!";
-  
+
   //send email
-  mail($admin_email, "$subject", $comment, "From:" . $email);
- 
+  mail($email, "$subject", $comment);
+
   }
 ?>
